34c8e4db3339f2cda12198fa161e15d93bdd9dbe
William Goulois <william.goulois@fabernovel.com>
2021-05-11 09:39:07 +0200

    nintendo page mobile wip

--- src/App.js
+++ src/App.js
@@ -1,160 +1,151 @@
-import { ReactComponent as Logo } from './assets/monogram/monogram.svg'
-import { ReactComponent as Bag } from './assets/monogram/bag.svg'
-import { ReactComponent as Menu } from './assets/monogram/menu.svg'
-import { ReactComponent as Instagram } from './assets/monogram/instagram.svg'
-import { ReactComponent as Twitter } from './assets/monogram/twitter.svg'
-import { ReactComponent as Facebook } from './assets/monogram/facebook.svg'
-import { ReactComponent as Youtube } from './assets/monogram/youtube.svg'
-import traveler from "./assets/monogram/traveler.jpg"
-import modulesHero from "./assets/monogram/modules-hero.jpg"
-import moduleImg from "./assets/monogram/module.jpg"
+import { ReactComponent as NintendoLogo } from './assets/nintendo/nintendo.svg'
+import { ReactComponent as LoginLogo } from './assets/nintendo/login.svg'
+import { ReactComponent as MenuLogo } from './assets/nintendo/menu.svg'
+import { ReactComponent as ArrowRight } from './assets/nintendo/arrow-right.svg'
+import { ReactComponent as HeartSpark } from './assets/nintendo/heart-spark.svg'
+import { ReactComponent as Calendar } from './assets/nintendo/calendar2.svg'
+import { ReactComponent as Players } from './assets/nintendo/players.svg'
+import { ReactComponent as Dpad } from './assets/nintendo/dpad.svg'
+import { ReactComponent as Publisher } from './assets/nintendo/publisher.svg'
+import { ReactComponent as Size } from './assets/nintendo/db.svg'
+import { ReactComponent as Lang } from './assets/nintendo/lang.svg'
+import hero from './assets/nintendo/hero.jpeg'
+import goldCoin from './assets/nintendo/gold-coin.png'
+import videoPlaceholder from './assets/nintendo/video-placeholder.jpeg'
+import screenshot1 from './assets/nintendo/screenshot01.jpeg'
+import screenshot2 from './assets/nintendo/screenshot02.jpeg'
+import screenshot3 from './assets/nintendo/screenshot03.jpeg'
+import screenshot4 from './assets/nintendo/screenshot04.jpeg'
 
 function App() {
   return (
     <>
       <header className="header">
-        <nav className="header__navbar">
-          <Logo />
+        <div className="header__nintendo-bg">
+          <NintendoLogo className="header__nintendo header__svg" />
+        </div>
+        <div className="header__right">
+          <LoginLogo className="header__login header__svg" />
+          <span className="header__login-text">Log in</span>
+          <MenuLogo className="header__menu header__svg" />
+        </div>
+      </header>
+      <div className="hero">
+        <img src={hero} alt="Big Minecraft lanscape" className="hero__img"></img>
+      </div>
+      <section className="buy">
+        <div className="container">
+          <div className="text underline">
+            Nintendo Switch
+          </div>
+          <div className="title">
+            Minecraft
+          </div>
+          <div className="text">
+            Available now
+          </div>
+          <div className="title">
+            $29.99
+          </div>
+          <a className="buy-digital" href="#b" alt="Buy digital copy of Minecraft">
+            <div className="buy-digital__text">Buy digital
+            </div>
+            <ArrowRight className="buy-digital__arrow-right" />
+          </a>
+          <div className="buy-points">
+            <img src={goldCoin} alt="Gold coin" className="buy-points__coin" />
+            <a className="buy-points__text" href="#b" alt="About gold points">Eligible for up to 150 points</a>
+          </div>
+          <a className="text-arrow" href="#b" alt="Buy physical copy of Minecraft">
+            <div className="text-arrow__text">Buy physical</div>
+            <ArrowRight className="text-arrow__arrow" />
+          </a>
+          <a className="add-wishlist" href="#b" alt="Add Minecraft to wishlist">
+            <HeartSpark className="add-wishlist__heart" />
+            <div className="add-wishlist__text">Add to wishlist</div>
+          </a>
+        </div>
+      </section>
+      <section className="carousel">
+        <div className="container">
+          <div className="carousel-grid"      >
+            <img className="carousel-grid__img carousel-grid__video" src={videoPlaceholder} alt="Minecraft video"></img>
+            <img className="carousel-grid__img" src={screenshot1} alt="Minecraft screenshot"></img>
+            <img className="carousel-grid__img" src={screenshot2} alt="Minecraft screenshot"></img>
+            <img className="carousel-grid__img" src={screenshot3} alt="Minecraft screenshot"></img>
+            <img className="carousel-grid__img" src={screenshot4} alt="Minecraft screenshot"></img>
+          </div>
+        </div>
+      </section>
+      <section className="description">
+        <div className="container">
+          <div className="presentation">
+            <div className="fade"></div>
+            <div className="title">Create, explore and survive alone or with friends.</div>
+            <div className="presentation__text">
+              <p>The new Minecraft is here! Featuring the latest game updates like Update Aquatic, cross-platform play and access to a multitude of content through the Minecraft Marketplace.</p>
 
-          <div className="header__right">
-            <Menu className="header__svg" />
-            <Bag className="header__svg" />
+              <p>Current owners of <a className="presentation__link" href="#m">Minecraft: Nintendo Switch Edition</a> will be able to upgrade to the new version of Minecraft by downloading it from the eShop for free!</p>
+
+              <p>After June 21st 2018, <a className="presentation__link" href="#m">"Minecraft: Nintendo Switch Edition" (2017)</a> will no longer be available for purchase from the Nintendo eShop. Existing owners can continue to play. *Existing owners will be able to re-download this version if needed.</p>
+
+              <p>Players of <a className="presentation__link" href="#m">"Minecraft: Nintendo Switch Edition" (2017)</a> will be able to convert their worlds to "Minecraft" (2018), but should expect the process to take some time. There is currently no way for Minecraft: Wii U Edition worlds to transfer to "Minecraft" (2018) on Nintendo Switch.</p>
+
+              <p>While The Super Mario Mash-Up Pack is pre-installed with the game, to access the remaining DLC packs included with the base game purchase you must download them from the in-game store.</p>
+
+              {/* TODO: Add Read more */}
+              {/* <p>A Microsoft Account is free and by signing in on device allows Minecraft players on Nintendo Switch to play with others on non-Nintendo devices. It also enables the portability of Minecoins and in-game store purchases to other devices.</p>
+
+              <p>You can purchase Minecoins in Nintendo eShop. Minecoins let you buy content in our in-game store, safely and securely. Minecoins are kept in your virtual wallet and can be used in Minecraft on other devices if you have a Microsoft Account.</p>
+
+              <p>Minecraft is a game about placing blocks and going on adventures.</p>
+
+              <p>Explore randomly generated worlds and build amazing things from the simplest of homes to the grandest of castles. Play in creative mode with unlimited resources or mine deep into the world in survival mode, crafting weapons and armor to fend off the dangerous mobs.</p> */}
+            </div>
+
+          </div>
+          <div className="specification">
+
+          </div>
+          <div className="play-modes">
+
+          </div>
+          <div className="rating">
+
+          </div>
+          <div className="online">
+
+          </div>
+
+          <small className="legal">
+
+          </small>
+
+        </div>
+      </section>
+      <section className="other-games">
+        <div className="container">
+          <div className="recommandation">
+            <div className="slider">
+              <ul className="game-list">
+                {/* <li className="game-card"></li> */}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer className="footer">
+        <nav className="footer__nav">
+          <div className="footer__links">
+            <div className="dropdown">
+              <div className="dropdown__title"></div>
+              <svg className="dropdown__svg"></svg>
+            </div>
           </div>
         </nav>
-      </header>
-
-      <div className="container">
-        <div className="product">
-          <img alt="" src={traveler} className="product__img"></img>
-          <div className="product__info">
-            <span className="product__title">Traveler Console [Pre-Order]</span>
-            <span className="product__price">$399.00</span>
-          </div>
-        </div>
-        <div className="product">
-          <img alt="" src={traveler} className="product__img"></img>
-          <div className="product__info">
-            <span className="product__title">Traveler Console [Pre-Order]</span>
-            <span className="product__price">$399.00</span>
-          </div>
-        </div>
-        <div className="product">
-          <img alt="" src={traveler} className="product__img"></img>
-          <div className="product__info">
-            <span className="product__title">Traveler Console [Pre-Order]</span>
-            <span className="product__price">$399.00</span>
-          </div>
-        </div>
-      </div>
-      <div className="hero-container">
-        <img alt="" src={modulesHero} className="hero-img"></img>
-      </div>
-      <div className="container">
-        <div className="product">
-          <img alt="" src={moduleImg} className="product__img"></img>
-          <div className="product__info">
-            <span className="product__title">Monogram Module [Pre-Order]</span>
-            <span className="product__price">$149.00</span>
-          </div>
-        </div>
-        <div className="product">
-          <img alt="" src={moduleImg} className="product__img"></img>
-          <div className="product__info">
-            <span className="product__title">Monogram Module [Pre-Order]</span>
-            <span className="product__price">$149.00</span>
-          </div>
-        </div>
-        <div className="product">
-          <img alt="" src={moduleImg} className="product__img"></img>
-          <div className="product__info">
-            <span className="product__title">Monogram Module [Pre-Order]</span>
-            <span className="product__price">$149.00</span>
-          </div>
-        </div>
-        <div className="product">
-          <img alt="" src={moduleImg} className="product__img"></img>
-          <div className="product__info">
-            <span className="product__title">Monogram Module [Pre-Order]</span>
-            <span className="product__price">$149.00</span>
-          </div>
-        </div>
-        <div className="product">
-          <img alt="" src={moduleImg} className="product__img"></img>
-          <div className="product__info">
-            <span className="product__title">Monogram Module [Pre-Order]</span>
-            <span className="product__price">$149.00</span>
-          </div>
-        </div>
-      </div>
-      <footer className="footer">
-        <div className="container">
-          <div className="newsletter">
-            <span className="newsletter__title">Subscribe to Monogram</span>
-            <p className="newsletter__description">Master productivity with Creative Console and get all the latest Monogram news</p>
-            <form className="newsletter__form">
-              <label for="email-input-field" className="newsletter__label">
-                <input className="newsletter__input" type="email" id="email-input-field" placeholder="Email Address"></input>
-              </label>
-              <div className="newsletter__submit">
-                <button type="submit" className="newsletter__button" disabled><span>Submit</span></button>
-              </div>
-            </form>
-          </div>
-          <div className="social">
-            <a href="#r" className="social__link"><Instagram className="social__svg" /></a>
-            <a href="#r" className="social__link"><Twitter className="social__svg" /></a>
-            <a href="#r" className="social__link"><Facebook className="social__svg" /></a>
-            <a href="#r" className="social__link"><Youtube className="social__svg" /></a>
-          </div>
-          <div className="sitemap">
-            <nav className="sitemap__nav">
-              <ul className="sitemap__groups">
-                <li className="sitemap__group">
-                  <div className="sitemap__group__title">Monogram</div>
-                  <address>305 King St. W.<br></br>Suite 502<br></br>Kitchener, ON<br></br>Canada</address>
-                </li>
-                <li className="sitemap__group">
-                  <div className="sitemap__group__title">Quick Links</div>
-                  <ul className="sitemap__group__ul">
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">Home</a></li>
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">How it Works</a></li>
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">Shop</a></li>
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">Download</a></li>
-                  </ul>
-                </li>
-                <li className="sitemap__group">
-                  <div className="sitemap__group__title">Help</div>
-                  <ul className="sitemap__group__ul">
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">FAQs</a></li>
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">Support</a></li>
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">Shipping and Sales</a></li>
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">Palette Support</a></li>
-                  </ul>
-                </li>
-                <li className="sitemap__group">
-                  <div className="sitemap__group__title">Information</div>
-                  <ul className="sitemap__group__ul">
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">About Us</a></li>
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">Work with us</a></li>
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">Privacy Policy</a></li>
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">Terms of Use</a></li>
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">Terms of Sale</a></li>
-                    <li className="sitemap__group__li"><a href="#l" className="sitemap__group__a">Press Kit</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <p className="copyright">
-            <div className="copyright__text">© Monogram 2021</div>
-            <div className="copyright__captcha">Protected by reCAPTCHA -</div>
-            <a href="#r" className="copyright__link">Privacy</a>
-            {" - "}
-            <a href="#r" className="copyright__link">Terms</a>
-          </p>
-        </div>
-
       </footer>
+      <div className="sticky-footer">
+      </div>
     </>
   );
 }
